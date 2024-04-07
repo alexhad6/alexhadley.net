58ffb60c0ce581848db5a225d88327eec7810812
Alex Hadley <alexhad6@users.noreply.github.com>
2024-04-06 22:38:19 -0700

    Add icon links, resume, and meta description

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header.astro";
+export { default as Icon } from "./Icon.astro";
+export { default as FontAwesomeIcon } from "./FontAwesomeIcon.astro";
+export { default as SimpleIconsIcon } from "./SimpleIconsIcon.astro";
+export { default as IconLink } from "./IconLink.astro";
